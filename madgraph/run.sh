f4ece9d35fbc2cac2d5241fe913002824987a09d
lvaughan <luke.vaughan@okstate.edu>
2025-04-14 16:09:56 -0500

    modify MG run options

--- madgraph/run.sh
+++ madgraph/run.sh
@@ -21,43 +21,46 @@ num_events_per_run=$4
 ###########################
 
 # Edit process card
-sed "s/output pp_tt_semi_full/output pp_tt_semi_full_${dataset_tag}/" include/proc_card_mg5.dat > proc_card.tmp
-if [ "$polarization" = "L" ]; then
-  sed -i "s/generate p p > t t~, t > b j j, t~ > b~ l- vl~/generate p p > t{L} t~, t > b j j, t~ > b~ l- vl~/" proc_card.tmp
+sed "s/OUTPUT_DIRECTORY/${dataset_tag}/" include/proc_card_mg5.dat > proc_card.tmp
+
+# Configure HHbbbb
+if [ "$process" = "HHbbbb" ]; then
+  sed -i "s/generate PROCESS/generate p p > h h QED<=2 [noborn=QCD]/" proc_card.tmp
+  sed -i "s/MODEL/loop_sm/" proc_card.tmp
 fi
-if [ "$polarization" = "R" ]; then
-  sed -i "s/generate p p > t t~, t > b j j, t~ > b~ l- vl~/generate p p > t{R} t~, t > b j j, t~ > b~ l- vl~/" proc_card.tmp
+
+# Configure bbbb from QCD
+if [ "$process" = "bbbb" ]; then
+  sed -i "s/generate PROCESS/generate p p > b b~ b b~/" proc_card.tmp
+  sed -i "s/MODEL/sm/" proc_card.tmp
+
+  min_ptb_filter=60 #GeV
+  sed -i "s/0.0  = ptb/$min_ptb_filter  = ptb/" ${dataset_tag}/Cards/run_card.dat
 fi
-if [ "$polarization" = "down" ]; then
-  sed -i "s/generate p p > t t~, t > b j j, t~ > b~ l- vl~/generate p p > t t~, t > b u d~, t~ > b~ l- vl~/" proc_card.tmp
-fi
-if [ "$polarization" = "charm" ]; then
-  sed -i "s/generate p p > t t~, t > b j j, t~ > b~ l- vl~/generate p p > t t~, t > b c s~, t~ > b~ l- vl~/" proc_card.tmp
+
+# Configure ttbar semiLeptonic
+if [ "$process" = "ttbar_semiLep" ]; then
+  sed -i "s/generate PROCESS/generate p p > t t~, t > b u d~, t~ > b~ l- vl~/" proc_card.tmp
+  sed -i "s/MODEL/sm/" proc_card.tmp
 fi
 
 # Edit run card
-#sed "s/\(.*\)= nevents\(.*\)/ $num_events = nevents\2/" include/run_card.dat > run_card.tmp
 sed "s/multi_run.*/multi_run $num_runs/" include/multi_run.config > multi_run.tmp
 sed -i "s/set nevents.*/set nevents $num_events_per_run/" multi_run.tmp
 
 # Run mg5_aMC binary on the process card
 ../submodules/mg5amcnlo-v3.5.5/bin/mg5_aMC proc_card.tmp
 
-# Copy the cuts.f card to the SubProcesses folder
-cp ./include/cuts.f "./pp_tt_semi_full_${dataset_tag}/SubProcesses/"
-
-# Copy the cards to the Cards folder
-#cp run_card.tmp "./pp_tt_semi_full_${dataset_tag}/Cards/run_card.dat"
-
 # Generate the Events!
 #"./pp_tt_semi_full_${dataset_tag}/bin/generate_events" -f
 echo "Please be patient while MadGraph generates processes..."
-"./pp_tt_semi_full_${dataset_tag}/bin/madevent" multi_run.tmp | tee "MadGraph_${dataset_tag}.log"
+"./${dataset_tag}/bin/madevent" multi_run.tmp | tee "MadGraph_${dataset_tag}.log"
 
 # Clean up workspace (generated automatically by madgraph binary)
 rm -f py.py
 rm -f MG5_debug
 rm -f ME5_debug
+rm -f additional_command
 rm *.tmp
 
 echo
