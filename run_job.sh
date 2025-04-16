5239bd727c46121c2653534b71af0cc3567b9175
lvaughan <luke.vaughan@okstate.edu>
2025-04-16 14:50:18 -0500

    first working pipeline

--- run_job.sh
+++ run_job.sh
@@ -17,7 +17,7 @@ source setup.sh
 if [ "$bypass_madgraph" = false ]; then
   start=`date +%s`
   cd madgraph
-  ./run.sh $madgraph_out_tag $process $num_runs $num_events_per_run $max_cpu_cores
+  ./run.sh $madgraph_out_tag $process $num_runs_madgraph $num_events_per_run $max_cpu_cores
   cd $WORKING_DIR
   end=`date +%s`
   runtime=$((end-start))
@@ -28,7 +28,7 @@ fi
 if [ "$bypass_pythia" = false ]; then
   start=`date +%s`
   cd pythia
-  ./run.sh $pythia_in_tag $pythia_out_tag $num_runs $max_cpu_cores
+  ./run.sh $pythia_in_tag $pythia_out_tag $num_runs_pythia $mu $minJetpT $max_cpu_cores
   cd $WORKING_DIR
   end=`date +%s`
   runtime=$((end-start))
@@ -39,16 +39,18 @@ fi
 if [ "$bypass_preprocessing" = false ]; then
   echo "Please be patient for Preprocessing..."
   start=`date +%s`
-  cd model
+  cd preprocessing
+  dir_preprocessing="WS_${preprocessing_out_tag}/logs"
+  dir_datasets="WS_${preprocessing_out_tag}/data"
+  mkdir -p "${dir_datasets}"
 
   job=0
   batch=1
-  for (( i=0 ; i<$num_runs ; i++ ));
+  for (( i=0 ; i<$num_runs_preprocessing; i++ ));
   do
     echo -e "\t\tSubmitting job to preprocess run $i"
     mkdir -p "${dir_preprocessing}/run_$i"
-    mkdir -p "${dir_datasets}/run_$i"
-    python -u Preprocessing.py $tag $i $dir_preprocessing $dir_datasets $analysis_type > "${dir_preprocessing}/run_$i/preprocessing.log" &
+    python -u preprocessing.py "../pythia/WS_${preprocessing_in_tag}/data/dataset_showered_run_${i}.root" "${dir_datasets}/dataset_preprocessed_run_$i.pkl" "$dir_preprocessing/run_$i" > "${dir_preprocessing}/run_$i/preprocessing.log" &
     job=$((job+1))
     if [ $job == $max_cpu_cores ]; then
       echo -e "\tStopping jobs submissions! Please wait for batch $batch to finish..."
@@ -63,36 +65,5 @@ if [ "$bypass_preprocessing" = false ]; then
   end=`date +%s`
   runtime=$((end-start))
   echo -e "\tPreprocessing Done!"
-  echo -e "\tTime (sec): $runtime"
-fi
-
-# Run batching to pad dataset
-if [ "$bypass_batch" = false ]; then
-  echo "Please be patient for Batching..."
-  start=`date +%s`
-  cd model
-
-  job=0
-  batch=1
-  for (( i=0 ; i<$num_runs ; i++ ));
-  do
-    echo -e "\t\tSubmitting job to pad and batch run $i"
-    mkdir -p "${dir_datasets}/logs"
-    python -u Batch_MSE.py $tag $i $dir_datasets > "${dir_datasets}/logs/batch.log" &
-    job=$((job+1))
-    if [ $job == $max_cpu_cores ]; then
-      echo -e "\tStopping jobs submissions! Please wait for batch $batch to finish..."
-      wait
-      job=0
-      batch=$((batch+1))
-    fi
-  done
-  wait
-
-  python -u Combine_Batches.py $tag $num_runs $dir_datasets
-  cd $WORKING_DIR
-  end=`date +%s`
-  runtime=$((end-start))
-  echo -e "\tBatching Done!"
   echo -e "\tTime (sec): $runtime"
 fi
